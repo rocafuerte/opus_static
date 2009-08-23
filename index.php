a5efc5b68789b560c5d68490a949601d0dbb71a3
Fabian Rios <info@rocafuerte.se>
2009-08-23 14:45:22 +0200

    menybar_rosa

--- index.php
+++ index.php
@@ -1,158 +1,179 @@
-<?php include "header_inc.php" ?>
+<?php include "header_inc.php" ?>	
  
   <div id="centerDiv">
   <div id="newsContent">
-<!--    <div class="newsActual">
-      <div class="newsHeader"><span class="smallGrey">03-16-2008 Fabian Rios</span><br />
-		<span class="newsHeaderFont">Close Ups</span>
+  <div class="newsTop"></div>
+<div class="newsActual">
+<div class="newsBg">
+      <div class="newsHeader">
+		<span class="newsHeaderFont">Salo och Tingstrand g&aring;r skilda v&auml;gar</span>
+        <span class="dateAuthor">03-16-2008 / </span><span class="dateAuthorPink">FABIAN RIOS</span>
       </div>
-      <div class="newsDescActual"><span class="boldGrey">Nu n&auml;r utvecklingspaketet till Ozzy sl&auml;ppts visar det sig sig att Close up &auml;r r&auml;dda f&ouml;r konkurrens och  har st&auml;ngt d&ouml;rrarna till en av Ozzys bilar.</span></div>
-     <div class="newsPicActual"><img src="images/news/festen_kan1_article_large.jpg" width="340" height="217"/></div>
-     <br />
-     MacWorld har tidigare uppm&auml;rksammat Greenpeaces lista &ouml;ver hur pass milj&ouml;medvetna diverse elektronikf&ouml;retag &auml;r. <br />
-     <br />
-I den sjunde upplagan av ‚Äú<a href="http://www.greenpeace.org/international/campaigns/toxics/electronics/how-the-companies-line-up">Guide to Greener Electronics</a>‚Äù  kl&auml;ttrar Apple upp till nionde plats, n&aring;got som inneb&auml;r att f&ouml;retaget  nu f&ouml;r f&ouml;rsta g&aring;ngen befinner sig p&aring; halvan &ouml;ver f&ouml;retag som sk&ouml;ter sig  hyfsat.<br />
-<br />
-J&auml;mf&ouml;rt med hur det s&aring;g ut f&ouml;r ett  &aring;r sedan har Apple blivit betydligt mer milj&ouml;medvetet. D&aring; rankades  Apple sist med 2,7 po&auml;ng av 10 m&ouml;jliga. Nu &auml;r po&auml;ngen uppe i 6,7 av 10. <br />
-<br />
-Bakom Apples uppg&aring;ng anges minskad anv&auml;ndning av farliga kemikalier, ett nytt &aring;tervinningsprogram och nya milj&ouml;v&auml;nliga Macbook Air.<br />
-<br />
-Allra mest milj&ouml;medvetna &auml;r man p&aring; Samsung och Toshiba som b&aring;da f&aring;r 7,7 po&auml;ng av Greenpeace.<br />
-<br />
-&Ouml;verl&auml;gset sist ligger Nintendo som f&aring;r ynka 0,3 po&auml;ng. Huvudorsaken till bottenplatsen &auml;r en total avsaknad av &aring;tervinningspolicy.<br />
-<br />
-<strong>H&auml;r &auml;r listan i sin helhet:</strong><br />
-<br />
-<ol>
-  <li>Samsung 7,7</li>
-  <li>Toshiba¬† 7,7</li>
-  <li>Nokia 7,3</li>
-  <li>Sony 7,3</li>
-  <li>Dell 7,3</li>
-  <li>Lenovo 7,3<br />
-  </li>
-  <li>Sony Ericsson 6,7</li>
-  <li>LG 6,7</li>
-  <li>Apple 6,7</li>
-  <li>Fujitsu-Siemens 6,7</li>
-  <li>HP 6,7</li>
-  <li>Motorola 6,3</li>
-  <li>Acer 5,7</li>
-  <li>Sharp 5,0</li>
-  <li>Panasonic 4,7</li>
-  <li>Microsoft 4,7</li>
-  </ol>
-<br />
-      
-
- </div>-->
-<table border="0" cellpadding="0" cellspacing="0" class="demoTable">
-      <tr>
-        <td width="80" class="noPadding" rowspan="3"><img src="images/news/start1.jpg" width="80" height="80" class="flowPic"  /></td>
-        <td width="270" class="tdPadding" colspan="2"><span class="boldGrey">Svenskt h&aring;rdrocksband</span></td>
-      </tr>
-      <tr>
-        <td class="tdPadding" colspan="2"><span class="smallGrey2">Lorem ipsum, lorem ipsum dolor sit amet</span></td>
-      </tr>
-      
-      <tr>
-      <td class="tdVertBottom"><span class="smallPink"><a href="#">L&auml;s mer</a></span></td>
-      <td align="right" class="tdVertBottom"><span class="smallGrey">2008-12-03</span></td>
-      </tr>
-    </table>
+      <div class="newsDescActual"><span class="smallBoldBlack">Nu n&auml;r utvecklingspaketet till Ozzy sl&auml;ppts visar det sig sig att Close up &auml;r r&auml;dda f&ouml;r konkurrens och  har st&auml;ngt d&ouml;rrarna till en av Ozzys bilar.</span></div>
+     <div class="newsPicActual"><img src="../opus_design/template/8625_getty_20080128162451.jpg" width="315" height="208"/> Foto: Random</div>
+     <div class="newsActualText">MacWorld har tidigare uppm&auml;rksammat Greenpeaces lista &ouml;ver hur pass milj&ouml;medvetna diverse elektronikf&ouml;retag &auml;r. <br />
+       <br />
+       I den sjunde upplagan av ì<a href="http://www.greenpeace.org/international/campaigns/toxics/electronics/how-the-companies-line-up">Guide to Greener Electronics</a>î  kl&auml;ttrar Apple upp till nionde plats, n&aring;got som inneb&auml;r att f&ouml;retaget  nu f&ouml;r f&ouml;rsta g&aring;ngen befinner sig p&aring; halvan &ouml;ver f&ouml;retag som sk&ouml;ter sig  hyfsat.<br />
+       <br />
+       J&auml;mf&ouml;rt med hur det s&aring;g ut f&ouml;r ett  &aring;r sedan har Apple blivit betydligt mer milj&ouml;medvetet. D&aring; rankades  Apple sist med 2,7 po&auml;ng av 10 m&ouml;jliga. Nu &auml;r po&auml;ngen uppe i 6,7 av 10. <br />
+       <br />
+       Bakom Apples uppg&aring;ng anges minskad anv&auml;ndning av farliga kemikalier, ett nytt &aring;tervinningsprogram och nya milj&ouml;v&auml;nliga Macbook Air.<br />
+       <br />
+       Allra mest milj&ouml;medvetna &auml;r man p&aring; Samsung och Toshiba som b&aring;da f&aring;r 7,7 po&auml;ng av Greenpeace.<br />
+       <br />
+       &Ouml;verl&auml;gset sist ligger Nintendo som f&aring;r ynka 0,3 po&auml;ng. Huvudorsaken till bottenplatsen &auml;r en total avsaknad av &aring;tervinningspolicy.
+ 
+</div>
+ </div>
+</div>
+<div class="newsActualBottom"></div>
 
 
-    <table border="0" cellpadding="0" cellspacing="0" class="demoTable">
-      <tr>
-        <td width="80" class="noPadding" rowspan="3"><img src="images/news/festen_kan1_article_large.jpg" width="80" height="51" class="flowPic" /></td>
-        <td width="270" class="tdPadding" colspan="2"><span class="boldGrey">Ossy ozbourne</span></td>
-       </tr>
-      <tr>
-        <td class="tdPadding" colspan="2"><span class="smallGrey2">Lorem ipsum, lorem ipsum dolor sit amet</span></td>
-      </tr>
-      
-      <tr>
-      <td class="tdVertBottom"><span class="smallPink"><a href="#">L&auml;s mer</a></span></td>
-      <td align="right"><span class="smallGrey">2008-12-03</span></td>
-      </tr>
-    </table>
+<div class="newsFlowTop"></div>
+<div class="newsFlow">
+<div class="bg"><img src="graphics/exempel/flute.png" width="168" height="222" />
+<div class="head"><span class="dateAuthor">03-16-2008 / </span><span class="dateAuthorPink">Fabian Rios</span></div>
+<div class="title"><span class="newsHeaderSmaller">Salo och Tingstrand g&aring;r skilda v&auml;gar</span></div>
+<div class="desc">Lorem ipsum, lorem ipsum dolor sit amet. Lorem ipsum, lorem ipsum dolor sit amet. Lorem ipsum, lorem ipsum dolor sit amet. Lorem ipsum, lorem ipsum dolor sit amet. </div>
+</div>
+</div>
+<div class="newsFlowBottom"></div>	
+
+<div class="newsFlowTop"></div>
+<div class="newsFlow2">
+<div class="bg">
+  <div class="head"><span class="dateAuthor">03-16-2008 / </span><span class="dateAuthorPink">FABIAN RIOS</span></div>
+<div class="title"><span class="newsHeaderSmaller">Salo och Tingstrand g&aring;r skilda v&auml;gar</span></div>
+<div class="desc">Lorem ipsum, lorem ipsum dolor sit amet. Lorem ipsum, lorem ipsum dolor sit amet. Lorem ipsum, lorem ipsum dolor sit amet. Lorem ipsum, lorem ipsum dolor sit amet. </div>
+</div>
+</div>
+<div class="newsFlowBottom"></div>	
+
+<div class="newsFlowTop"></div>
+<div class="newsFlow3">
+<img src="../opus_design/template/8625_getty_20080128162451.jpg" width="343" height="226"/><br />
+<div class="bg">
+<div class="head"><span class="dateAuthor">03-16-2008 / </span><span class="dateAuthorPink">Fabian Rios</span></div>
+<div class="title"><span class="newsHeaderSmaller">Salo och Tingstrand g&aring;r skilda v&auml;gar</span></div>
+<div class="desc">Lorem ipsum, lorem ipsum dolor sit amet. Lorem ipsum, lorem ipsum dolor sit amet. Lorem ipsum, lorem ipsum dolor sit amet. Lorem ipsum, lorem ipsum dolor sit amet. </div>
+</div>
+</div>
+<div class="newsFlowBottom"></div>	
 
 
-    <table border="0" cellpadding="0" cellspacing="0" class="demoTable">
-      <tr>
-        <td class="tdPadding" colspan="2"><span class="boldGrey">Hammerfall</span></td>
-      </tr>
-      <tr>
-        <td class="tdPadding" colspan="2"><span class="smallGrey2">Lorem ipsum, lorem ipsum dolor sit amet</span><span class="smallGrey2">Lorem ipsum, lorem ipsum dolor sit amet</span></td>
-      </tr>
-      
-      <tr>
-      <td width="173" class="tdVertBottom"><span class="smallPink"><a href="#">L&auml;s mer</a></span></td>
-      <td width="299" align="right"><span class="smallGrey">2008-12-03</span></td>
-      </tr>
-    </table>
 
-
-    <table border="0" cellpadding="0" cellspacing="0" class="demoTable">
-      <tr>
-        
-        <td width="270" class="tdPadding" colspan="2"><span class="boldGrey">Ossy ozbourne</span></td>
-       </tr>
-      <tr>
-        <td class="tdPadding" colspan="2"><span class="smallGrey2">Lorem ipsum, lorem ipsum dolor sit amet</span></td>
-      </tr>
-      
-      <tr>
-      <td class="tdVertBottom"><span class="smallPink"><a href="#">L&auml;s mer</a></span></td>
-      <td align="right"><span class="smallGrey">2008-12-03</span></td>
-      </tr>
-    </table>
-
-
-    <table border="0" cellpadding="0" cellspacing="0" class="demoTable">
-      <tr>
-        <td width="80" class="noPadding" rowspan="3"><img src="images/videothumbs/hammerfall.JPG" width="80" height="52" class="flowPic" /></td>
-        <td width="270" class="tdPadding" colspan="2"><span class="boldGrey">Hammerfall</span></td>
-      </tr>
-      <tr>
-        <td class="tdPadding" colspan="2"><span class="smallGrey2">Lorem ipsum, lorem ipsum dolor sit amet</span></td>
-      </tr>
-      
-      <tr>
-      <td class="tdVertBottom"><span class="smallPink"><a href="#">L&auml;s mer</a></span></td>
-      <td align="right"><span class="smallGrey">2008-12-03</span></td>
-      </tr>
-    </table>
-
-
-    <table border="0" cellpadding="0" cellspacing="0" class="demoTable">
-      <tr>
-        <td width="80" class="noPadding" rowspan="3"><img src="images/news/FATAL-SMILE-PHOTO.jpg" width="80" height="84" class="flowPic"  /></td>
-        <td width="270" class="tdPadding" colspan="2"><span class="smallGrey2">Lorem ipsum</span></td>
-      </tr>
-      <tr>
-        <td class="tdPadding" colspan="2"><span class="smallGrey2">Lorem ipsum, lorem ipsum dolor sit amet</span></td>
-      </tr>
-      
-      <tr>
-      <td class="tdVertBottom"><span class="smallPink"><a href="#">L&auml;s mer</a></span></td>
-      <td align="right"><span class="smallGrey">2008-12-03</span></td>
-      </tr>
-    </table>
-
-    <div id="descent">
-      <ul>
-        <li> <span class="descentDate">2008-02-30</span><br />
+<div class="newsFlowTop"></div>
+<div class="newsFlow2">
+<div class="bg">
+<div id="descent">
+  <ul>
+        <li> <span class="dateAuthor">2008-02-30</span><br />
           <span class="boldPink"><a href="#">Antalet h&aring;rdrockare i forshaga &ouml;kar drastiskt</a></span> </li>
-        <li> <span class="descentDate">2008-02-24</span><br />
+        <li> <span class="dateAuthor">2008-02-24</span><br />
           <span class="boldPink"><a href="#">Nya chefen f&ouml;r ica heter per</a></span> </li>
-        <li> <span class="descentDate">2008-02-13</span><br />
+        <li> <span class="dateAuthor">2008-02-13</span><br />
           <span class="boldPink"><a href="#">Closeup &auml;r sveriges st&ouml;rsta h&aring;rdrockstidning!</a></span> </li>
       </ul>
     </div>
+
+</div>
+</div>
+<div class="newsFlowBottom"></div>	
+
     </div></div>
  
 	
 
+<!-- </div>centerDiv-->
+
+<div class="centerDivFooterSmall"></div>
+</div>
+<div class="rightCenterContent">
+<div class="rightCenterDivTop"></div>
+<div class="rightCenterDiv">
+<div class="rightCenterDivBg">
+  <!-- searchDiv -->
   
-  <?php include "footer.php" ?>
+  <br />
+
+  <div class="rightCenterDivFlow"><a href="tv.php"> <span class="righCenterDivTitle">OPUS TV </span></a><br />
+ <object width="159" height="126"><param name="movie" value="http://www.youtube.com/v/YjO_VXHxsRw&hl=sv&fs=1&"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/YjO_VXHxsRw&hl=sv&fs=1&" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="159" height="126"></embed></object><br />
+    <span class="smallGrey">PÂ Opus TV kan du kolla pÂ spelningar och andra videoklipp<br />
+   <a href="tv.php">OPUS TV</a></span> <br />
+    <br />
+    <hr class="hr2" noshade="noshade" />
+  </div>
+  <!-- rightCenterDivFlow -->
+ 
+   <div class="rightCenterDivFlow"><a href="tv.php"> <span class="righCenterDivTitle">T‰vla och vinn</span></a><br />
+   <img src="graphics/exempel/compete.png" width="159" height="113" />
+    <span class="smallGrey">Kan du svaret? skicka in det och fÂ chansen att vinna!<br />
+   <a href="tv.php">T‰vla!</a></span> <br />
+    <br />
+  </div>
+  <!-- rightCenterDivFlow -->
+  </div>
+  <!-- rightCenterDivBg -->
+  
+</div>
+<!-- righCenterDiv -->
+<div class="rightCenterDivFooter"></div>
+
+<div class="rightCenterDivTop"></div>
+<div class="rightCenterDiv">
+<div class="rightCenterDivBg">
+  <!-- searchDiv -->
+  
+  <br />
+
+  <div class="rightCenterDivFlow"><a href="tv.php"> <span class="righCenterDivTitle">OPUS TV </span></a><br />
+ <object width="159" height="126"><param name="movie" value="http://www.youtube.com/v/YjO_VXHxsRw&hl=sv&fs=1&"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/YjO_VXHxsRw&hl=sv&fs=1&" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="159" height="126"></embed></object>
+    <span class="smallGrey">PÂ Opus TV kan du kolla pÂ spelningar och andra videoklipp<br />
+   <a href="tv.php">OPUS TV</a></span> <br />
+    <br />
+    <hr class="hr2" noshade="noshade" />
+  </div>
+  <!-- rightCenterDivFlow -->
+ 
+   <div class="rightCenterDivFlow"><a href="tv.php"> <span class="righCenterDivTitle">T‰vla och vinn</span></a><br />
+   <img src="graphics/exempel/compete.png" width="164" height="117" />
+    <span class="smallGrey">Kan du svaret? skicka in det och fÂ chansen att vinna!<br />
+   <a href="tv.php">T‰vla!</a></span> <br />
+    <br />
+  </div>
+  <!-- rightCenterDivFlow -->
+  </div>
+  <!-- rightCenterDivBg -->
+  
+</div>
+<!-- righCenterDiv -->
+<div class="rightCenterDivFooter"></div>
+</div>
+<!-- rightCenterContent-->
+<div id="rightBannerDiv">
+  <!--<img src="graphics/annonspil.gif" alt="Annonser"/> -->
+
+  <a href="#" target="_blank"> <img src="graphics/exempel/banner.png" class="rightBanners" width="250" alt=""/> </a>
+</div>
+<!--rightBannerDiv-->
+
+<div id="footer"> P.O. Box 4411 SE-102 69 Stockholm   Sweden |  + 46-8 462 02 14|  mail@closeupmagazine.net | Rocafuerte design
+</div>
+</div>
+<!-- pagecell -->
+
+
+<!-- footer -->
+<br />
+<br />
+<br />
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-3996201-3");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
+</body></html>
