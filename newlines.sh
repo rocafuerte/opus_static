372b871646e5184d0e6b4486a0a23d815fea46b3
Felix Rios <felixrios@laputa.local>
2009-09-05 10:56:08 +0200

    newlines fix

--- newlines.sh
+++ newlines.sh
@@ -5,10 +5,12 @@ if [ "$1" = "" ] ; then
     exit 1;
 fi
 
+cat $1 > $1.backup
+
 echo -e "Fix newlines"
 sed \
     -e 's/>/>\
 /g' \
     -e 's/[ ]*//g' \
-    $1 > $1.sed && echo "OK" || exit 1 
-echo -e "File saved as $1.sed"
+    $1.backup > $1 && echo "OK" || exit 1 
+echo -e "Backup File saved as $1.backup"
