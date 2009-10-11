5c29ac34ab976717486fdb202270f675ae7e815e
Fabian Rios <info@rocafuerte.se>
2009-10-11 16:21:14 +0200

    changed the Tidigare Utgavor

--- number.php
+++ number.php
@@ -2,66 +2,39 @@
 
 <div id="centerDiv">
 <div class="newsBg">
-<span class="newsHeaderFont">Close-Up shop</span><br /><br />
+<span class="newsHeaderFont">OPUS Butik</span><br /><br />
 
 <span class="smallPink">
 <p><a href="prenumeration.php">Prenumeration</a> | <a href="number.php"><strong>Tidigare utgåvor</strong></a> | <a href="shop.php">OPUS Prylar</a></p>
-</span><br />
+</span>
+<br />
 <hr />
-
 <br />
 
-		<div class="newsHeader"><p>
-		<span class="newsHeaderSmaller">#23</span> 
-       <span class="smallPink"><a href="#">79kr Köp</a></span>
-		</p>
-        </div>
-		<div class="newsPicActual">
-		<img src="graphics/exempel/monthly_mag.jpg" />
-		 </div>
-		<div class="textContainer">
-        I #113 finns intervjuer med Pearl Jam, Rammstein, Behemoth, Raised Fist, Suffocation, August Burns Red, Municipal Waste, Caliban, Gatans Lag, Gallows, Tenet, Merauder, Job For A Cowboy, Construcdead, Arabrot, Augury, Bone Gnawer, Stay Hungry, Switch Opens, Goatwhore, Spinnerette, Leprous, Pyramido, Swashbuckle, Arkaea, I Wrestled A Bear Once, One Without, Nazxul och Ocean Chief.
+<div class="tidigareFlow">
+  <img src="graphics/exempel/monthly_mag.jpg" width="113" height="135" />
+  <div class="title"> Nummer 23 - <span class="smallPink"><a href="#">79kr Köp</a></span>
+</div>
+  <div class="description">
+    Lite information om just dethär numret,<a href="#"> texten</a> kommer inte att bli "högre" än vad själva bilden är för att hålla en snygg design.
+  </div>
+  <div class="buy">   
+  </div>
+</div>
 
-Stor festivalspecial med recensioner från Metaltown, West Coast Riot, Peace & Love, Sweden Rock, Rockweekend, Hellfest, Roskildefestivalen, Tuska och Sonisphere. 
-
-Dessutom recenseras som vanligt mängder av skivor, dvd:s, demos och filmer. 
-
-Din mentala metalstatus testar du genom att lösa det kluriga korsordet och frågehörnan Brainwatch.
-        </div>
-	
-
-
-
- <div class="textContainer">
- <p> <span class="newsHeaderSmaller">Gamla nummer</span></p><br />
-<span class="smallGrey2"><em>
-<strong>Fyll i namn och adressuppgifter i meddelandef&auml;ltet 
-vid Paysonbetalningen<br /><br />
-Har du inget <a href="https://www.payson.se/account/signup/" target="_self">Paysonkonto?</a><br />
-Ta dig 10 sekunder och &ouml;ppna ett kostnadsfritt!
-</strong>
-</em></span></div><br /><br />
-
-<table width="100%" border="0" cellpadding="0" cellspacing="0" class="demoTable">
-  <tr>
-    <td class="trBorderBottom" width="150"><span class="smallPink"><a href="#">#22</a>
-	<a href="#">80kr</a>
-	</span></td>
-
-	<td class="trBorderBottom"><span class"smallGrey">&nbsp;</span></td>
-	
-<td class="trBorderBottom"align="right">
-<span class="smallPink"><a href="#"> Visa</a></span>
-<span class="smallPink"> / <a href="#"> K&ouml;p</a></span>
-
-</td></tr>
-</table>
+<div class="tidigareFlow">
+  <img src="graphics/exempel/monthly_mag.jpg" width="113" height="135" />
+  <div class="title"> Nummer 23 - <span class="smallPink"><a href="#">79kr Köp</a></span>
+</div>
+  <div class="description">
+    Lite information om just dethär numret, texten kommer inte att bli "högre" än vad själva bilden är för att hålla en snygg design.
+  </div>
+  <div class="buy">   
+  </div>
+</div>
 
 
 
-<a href="http://payson.se" target="_blank">
-<img src="graphics/QuickAgentHeader-smaller.gif" width="313" height="57" border="0"/>
-</a>
 </div>
 </div>
 <?php include "footer.php" ?>
